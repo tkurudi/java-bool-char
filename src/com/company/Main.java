8a550b52420da8978bdf91e8312526e8fe1f8b57
tkurudi <t.kurudi209@gmail.com>
2019-09-09 15:35:48 +1200

    first

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+        char myUniCode = '\u0044';
+        //both with print d
+
+        boolean myTrueBoolVal = true;
+        boolean myFalseBoolVal = false;
+
+        boolean isCustOver21 = true;
+    }
+}
